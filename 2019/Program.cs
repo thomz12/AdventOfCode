a981835ca34f2a9f7a5a6e00196a93e939386db3
Thom Zeilstra <thomz12@hotmail.nl>
2019-12-02 11:15:32 +0100

    Program is now able to run a given day, or the current day.

--- 2019/Program.cs
+++ 2019/Program.cs
@@ -18,8 +18,40 @@ namespace _2019
         /// <param name="args">Unused command line arguments.</param>
         static void Main(string[] args)
         {
-            // Run the puzzles of the day provided here.
-            AOCRunner runner = new AOCRunner(new Day02(), RunMode.BOTH);
+            AOCDay day = null;
+
+            // Try to get the given or current day to run.
+            try
+            {
+                // If an argument is given, run that day.
+                if (args.Length == 1)
+                {
+                    Console.WriteLine($"Getting puzzle solution for day {args[0]}");
+                    Type type = GetTypeByDay(int.Parse(args[0]));
+                    day = (AOCDay)Activator.CreateInstance(type);
+                }
+                // Othwerwise, the current day of the month.
+                else
+                {
+                    Console.WriteLine("Getting the current day...");
+                    Type type = GetTypeByDay(DateTime.Now.Day);
+                    day = (AOCDay)Activator.CreateInstance(type);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to create or find given day.");
+                Console.WriteLine(ex);
+            }
+
+            // Run the puzzles of the day.
+            AOCRunner runner = new AOCRunner(day, RunMode.BOTH);
+        }
+
+        private static Type GetTypeByDay(int day)
+        {
+            string dayString = day.ToString().PadLeft(2, '0');
+            return Type.GetType($"_2019.Day{dayString}");
         }
     }
 }
