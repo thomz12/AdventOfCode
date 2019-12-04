c7656deeb6835bac1d0d6112bcdc62392bf59442
Thom Zeilstra <thomz12@hotmail.nl>
2019-12-04 11:37:58 +0100

    Day 04

--- 2019/Day04.cs
+++ 2019/Day04.cs
@@ -1,4 +1,6 @@
 ﻿using AOCHelper;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _2019
 {
@@ -23,7 +25,22 @@ namespace _2019
         /// <returns>The answer.</returns>
         public override object Puzzle1(AOCInput input)
         {
-            return null;
+            int[] range = input.GetInput("-").Select(x => int.Parse(x)).ToArray();
+
+            int count = 0;
+
+            for(int i = range[0]; i < range[1]; ++i)
+            {
+                string password = i.ToString();
+
+                bool ascending = password.Zip(password.Skip(1), (a, b) => a <= b).All(x => x);
+                bool same = password.Zip(password.Skip(1), (a, b) => a == b).Any(x => x);
+
+                if (same && ascending)
+                    count++;
+            }
+
+            return count;
         }
 
         /// <summary>
@@ -33,7 +50,35 @@ namespace _2019
         /// <returns>The answer.</returns>
         public override object Puzzle2(AOCInput input)
         {
-            return null;
+            int[] range = input.GetInput("-").Select(x => int.Parse(x)).ToArray();
+
+            int count = 0;
+
+            for (int i = range[0]; i < range[1]; ++i)
+            {
+                string password = i.ToString();
+
+                bool ascending = password.Zip(password.Skip(1), (a, b) => a <= b).All(x => x);
+                bool same = false;
+
+                for (int j = 0; j < password.Length - 1; ++j)
+                {
+                    if (password[j] == password[j + 1])
+                    {
+                        if ((j + 2 >= password.Length || password[j] != password[j + 2]) && 
+                            (j - 1 < 0 || password[j] != password[j - 1]))
+                        {
+                            same = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (same && ascending)
+                    count++;
+            }
+
+            return count;
         }
     }
 }
