b8638e0206eb8c707c378d67f3eaffea58a76e59
Thom Zeilstra <thomz12@hotmail.nl>
2019-12-06 10:52:54 +0100

    Added remaining days.

---
+++ 2019/Day19.cs
@@ -0,0 +1,39 @@
+﻿using AOCHelper;
+
+namespace _2019
+{
+    /// <summary>
+    /// Day of the advent of code 2019, containing solutions of the two puzzles.
+    /// </summary>
+    public class Day19 : AOCDay
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Day19() 
+            : base(2019, 19)
+        {
+
+        }
+
+        /// <summary>
+        /// The first puzzle.
+        /// </summary>
+        /// <param name="input">The AOCInput object for getting the input.</param>
+        /// <returns>The answer.</returns>
+        public override object Puzzle1(AOCInput input)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// The second puzzle.
+        /// </summary>
+        /// <param name="input">The AOCInput object for getting the input.</param>
+        /// <returns>The answer.</returns>
+        public override object Puzzle2(AOCInput input)
+        {
+            return null;
+        }
+    }
+}
---
+++ 2019/Day25.cs
@@ -0,0 +1,39 @@
+﻿using AOCHelper;
+
+namespace _2019
+{
+    /// <summary>
+    /// Day of the advent of code 2019, containing solutions of the two puzzles.
+    /// </summary>
+    public class Day25 : AOCDay
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Day25() 
+            : base(2019, 25)
+        {
+
+        }
+
+        /// <summary>
+        /// The first puzzle.
+        /// </summary>
+        /// <param name="input">The AOCInput object for getting the input.</param>
+        /// <returns>The answer.</returns>
+        public override object Puzzle1(AOCInput input)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// The second puzzle.
+        /// </summary>
+        /// <param name="input">The AOCInput object for getting the input.</param>
+        /// <returns>The answer.</returns>
+        public override object Puzzle2(AOCInput input)
+        {
+            return null;
+        }
+    }
+}
