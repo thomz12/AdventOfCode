295a332e8ddcc30810b1499292c153df6c42bba3
Thom Zeilstra <thomz12@hotmail.nl>
2019-12-08 11:29:32 +0100

    Day 08

--- 2019/Day08.cs
+++ 2019/Day08.cs
@@ -1,4 +1,6 @@
 ﻿using AOCHelper;
+using System;
+using System.Linq;
 
 namespace _2019
 {
@@ -23,7 +25,24 @@ namespace _2019
         /// <returns>The answer.</returns>
         public override object Puzzle1(AOCInput input)
         {
-            return null;
+            int[] data = input.GetInput().ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+
+            int width = 25;
+            int height = 6;
+
+            int[][] layers = new int[data.Length / (width * height)][];
+
+            // Copy layer data.
+            for (int i = 0; i < layers.Length; ++i)
+            {
+                layers[i] = new int[width * height];
+                Array.Copy(data, i * (width * height), layers[i], 0, width * height);
+            }
+
+            // Get layer with least zeroes.
+            int[] leastZeroes = layers.OrderBy(image => image.Count(x => x == 0)).First();
+
+            return leastZeroes.Count(x => x == 1) * leastZeroes.Count(x => x == 2);
         }
 
         /// <summary>
@@ -33,7 +52,45 @@ namespace _2019
         /// <returns>The answer.</returns>
         public override object Puzzle2(AOCInput input)
         {
-            return null;
+            int[] data = input.GetInput().ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+
+            int width = 25;
+            int height = 6;
+
+            int[][] layers = new int[data.Length / (width * height)][];
+
+            // Copy layer data.
+            for (int i = 0; i < layers.Length; ++i)
+            {
+                layers[i] = new int[width * height];
+                Array.Copy(data, i * (width * height), layers[i], 0, width * height);
+            }
+
+            int[] image = new int[width * height];
+
+            // Get image data by combining the layers.
+            for (int i = 0; i < width * height; ++i)
+            {
+                for(int j = 0; j < layers.Length; ++j)
+                {
+                    if(layers[j][i] != 2)
+                    {
+                        image[i] = layers[j][i];
+                        break;
+                    }
+                }
+            }
+
+            // Visualize the image in the console.
+            for(int i = 0; i < height; ++i)
+            { 
+                for(int j = 0; j < width; ++j)
+                    Console.Write(image[j + width * i] == 0 ? " " : "X");
+
+                Console.WriteLine();
+            }
+
+            return "See console ouptut.";
         }
     }
 }
